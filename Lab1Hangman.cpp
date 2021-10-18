81cc3903f5858c31946feb96c5bcd9c005c5131b
BigMikeonaTrike <89423832+BigMikeonaTrike@users.noreply.github.com>
2021-10-18 16:42:28 +1300

    finished WITH comments

--- Lab1Hangman.cpp
+++ Lab1Hangman.cpp
@@ -4,30 +4,32 @@
 
 using namespace std;
 
-void welcomePrint();
-char randomLetter(char wordLetters[6], int temp);
-void hangmanPrint(int lives, char wordLetters[6], bool correctGuess = false);
-bool playerGuess(int lives, char goal);
-
+// define functions
+void welcomePrint(); // wihtout parameters AND no return value
+char randomLetter(char wordLetters[6], int temp); // with parameters AND return value
+void hangmanPrint(int lives, char wordLetters[6], bool correctGuess = false); // with parameters AND no return value
+bool playerGuess(int lives, char goal); // with parameters AND assigned to variable
 
 
 int main()
 {
-	char wordLetters[6] = { 'Y','o','o','b','e', 'e' };
+	//initialize variables
+	char wordLetters[6] = { 'y','o','o','b','e', 'e' };
 	int livesRemain = 3, temp = 0;
 	bool correct = false;
+	//game start
 	welcomePrint();
-	char goalLetter = randomLetter(wordLetters, temp);
+	char goalLetter = randomLetter(wordLetters, temp); // selects goal letter
 	cout << goalLetter;
-	hangmanPrint(livesRemain, wordLetters);
-a:
+	hangmanPrint(livesRemain, wordLetters); // prints hangman
+a:												
 	correct = playerGuess(livesRemain, goalLetter);
 	if (correct == false) {
 		livesRemain--;
 		cout << "\nIncorrect Guess, Try Again\nlives remaining: " << livesRemain << endl;
 	}
-	if (livesRemain > 0  && correct == false) {
-		goto a;
+	if (livesRemain > 0  && correct == false) { // repeats guess until correct or no lives remain
+		goto a; 
 	}
 	if (correct == true) {
 		cout << "\nCorrect Guess, Congratulations!\n";
@@ -35,10 +37,10 @@ a:
 	else {
 		cout << "\nOut Of Lives, You Lose\nBetter Luck Next Time!\n";
 	}
-	hangmanPrint(livesRemain, wordLetters, correct);
+	hangmanPrint(livesRemain, wordLetters, correct); // prints final hangman
 }
 
-bool playerGuess(int lives, char goal) {
+bool playerGuess(int lives, char goal) { // asks & gets users guess, returns whether correct
 	char input;
 	cout << "\nGuess the missing letter: ";
 	cin >> input;
@@ -50,7 +52,7 @@ bool playerGuess(int lives, char goal) {
 	}
 }
 
-void welcomePrint() { // without parameters
+void welcomePrint() { // prints outs welcome message and line
 	cout << "Welcome to Hangman. This game is to check your spelling skill\n";
 	for (int i = 0; i < 62; i++) {
 		cout << "=";
@@ -58,7 +60,7 @@ void welcomePrint() { // without parameters
 	cout << endl;
 }
 
-char randomLetter(char wordLetters[6], int temp) {
+char randomLetter(char wordLetters[6], int temp) { // chooses and removes random letter to be goal
 	srand(time(0));
 	temp = rand() % 5; 
 	char tempLetter = wordLetters[temp]; 
@@ -66,20 +68,20 @@ char randomLetter(char wordLetters[6], int temp) {
 	return tempLetter;
 }
 
-void hangmanPrint(int lives, char wordLetters[6], bool correctGuess) { // with parameters
-	string wholeBody[2][3] = { {"\\0/", " |", "/ \\"}, {" X", "/|\\", "| |"} };
+void hangmanPrint(int lives, char wordLetters[6], bool correctGuess) { // prints hangman in different states depending on whether game active, player won or player lost
+	string wholeBody[2][3] = { {"\\0/", " |", "/ \\"}, {" X", "/|\\", "| |"} }; // stores alive and dead hangman
 
 	cout << "\n  ==============";
 	cout << "\n  ||       |";
-	if (lives <= 0) {
-		for (int i = 0; i < 3; i++) {
+	if (lives <= 0) { // if game over prints dead state
+		for (int i = 0; i < 3; i++) { 
 			cout << "\n  ||      " << wholeBody[1][i];
 		}
 	}
 	else {
 		for (int i = 0; i < 3; i++) {
 			cout << "\n  ||      ";
-			if (correctGuess == true) {
+			if (correctGuess == true) { // moves hangman to right for 'Walkman' state else print hanging state
 				for (int j = 0; j < 5; j++) {
 					cout << "  ";
 				}
@@ -90,8 +92,8 @@ void hangmanPrint(int lives, char wordLetters[6], bool correctGuess) { // with p
 	cout << "\n  ||===|       |===========| ";
 	cout << "\n  ||                       |";
 	cout << "\n  ||    '";
-	if (lives == 0) {
-		cout << "Yoobee";
+	if (lives == 0 || correctGuess == true) { // if game over prints full word without missing letter
+		cout << "yoobee";
 	}
 	else {
 		for (int i = 0; i < 6; i++) {
